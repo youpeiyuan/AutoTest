ae4ac8de189b308776b99615b271fd40f43a54f4
youpeiyuan <532931038@qq.com>
2021-07-26 16:33:22 +0800

    Chapter6

--- Chapter6/src/main/java/com/tester/extented/exreports/reportdemo.java
+++ Chapter6/src/main/java/com/tester/extented/exreports/reportdemo.java
@@ -1,8 +1,25 @@
 package com.tester.extented.exreports;
 
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
 public class reportdemo {
-
+    @Test
     public void test1(){
-
+        Assert.assertEquals(1,2);
+    }
+    @Test
+    public void test2(){
+        Assert.assertEquals(1,1);
+    }
+    @Test
+    public void test3(){
+        Assert.assertEquals("aaa","aaa");
+    }
+    @Test
+    public void logdemo(){
+        Reporter.log("这是我自己写的log日志");
+        throw new RuntimeException("这是我自己的运行时异常");
     }
 }
