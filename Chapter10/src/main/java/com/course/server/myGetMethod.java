7c5e1c17e0996000ca8f2af5999bf2d78a4e79c3
youpeiyuan <532931038@qq.com>
2021-08-01 16:58:44 +0800

    cookiesgetmethod

--- Chapter10/src/main/java/com/course/server/myGetMethod.java
+++ Chapter10/src/main/java/com/course/server/myGetMethod.java
@@ -5,7 +5,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Objects;
 
 @RestController
 public class myGetMethod {
@@ -16,5 +18,23 @@ public class myGetMethod {
         Cookie cookies = new Cookie("login","true");
         response.addCookie(cookies);
         return "恭喜你获取cookies成功";
+    }
+
+
+    /**
+     * 要求客户端携带cookies进行访问
+     */
+    @RequestMapping(value = "/get/cookies",method = RequestMethod.GET)
+    public String getwithcookies(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        if (Objects.isNull(cookies)){
+            return "必须携带cookies才能访问";
+        }
+        for (Cookie cookie : cookies){
+            if (cookie.getName().equals("login") && cookie.getValue().equals("true")){
+                return "这是需要携带cookies信息才能访问的Get请求";
+            }
+        }
+        return "cookies error";
     }
 }
